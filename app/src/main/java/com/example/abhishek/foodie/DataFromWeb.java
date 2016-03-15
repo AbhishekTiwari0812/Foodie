f281ee4551cec67f917d07b847583ca045859356
Abhishek <abhishektiwari08121994007@gmail.com>
2016-03-15 14:48:29 +0530

    fixed the Manager page to ask for password.

--- app/src/main/java/com/example/abhishek/foodie/DataFromWeb.java
+++ app/src/main/java/com/example/abhishek/foodie/DataFromWeb.java
@@ -3,9 +3,9 @@ package com.example.abhishek.foodie;
 /**
  * Created by Abhishek on 14-03-2016.
  */
+//Just a prototype
 public class DataFromWeb {
-    private static String manager_login_password="1234";
-
+    private static String manager_login_password = "1234";
 
     public static String get_manager_password() {
         return manager_login_password;
